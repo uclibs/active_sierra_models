7d78a8af55692b42bb01ac3b8282e298f3a018f7
James Van Mil <james.vanmil@gmail.com>
2014-07-11 13:07:55 -0400

    Update require to include all models

--- lib/active_sierra_models.rb
+++ lib/active_sierra_models.rb
@@ -1,8 +1,25 @@
 require "pg"
 require "active_record"
+
 require "active_sierra_models/version"
-require "models/sierra_table"
+
+require "models/bib_record"
+require "models/bib_record_item_record_link"
+require "models/bib_record_order_record_link"
 require "models/bib_view"
+require "models/checkout"
+require "models/item_record"
+require "models/item_record_property"
+require "models/item_view"
+require "models/order_record"
+require "models/order_view"
+require "models/record_metadata"
+require "models/resource_record"
+require "models/resource_view"
+require "models/sierra_table"
+require "models/subfield"
+require "models/subfield_view"
+require "models/varfield_view"
 
 module ActiveSierraModels
 end
