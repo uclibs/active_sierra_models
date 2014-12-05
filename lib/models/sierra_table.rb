83ec3485312803097729963a6b670e27af1b8bc7
James Van Mil <james.vanmil@gmail.com>
2014-12-05 11:30:22 -0500

    Update models to match active_sierra develop branch

--- lib/models/sierra_table.rb
+++ lib/models/sierra_table.rb
@@ -1,5 +1,4 @@
 class SierraTable < ActiveRecord::Base
   self.abstract_class = true
-  ActiveRecord::Base.configurations = YAML::load(IO.read('config/database.yml'))
   establish_connection :sierra
 end
